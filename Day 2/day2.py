21182a31376432903cff10b049dea24079976238
AlexSpeedruns <44741588+AlexSpeedruns@users.noreply.github.com>
2022-12-02 00:23:34 -0500

    Day 2
    Day 2 code

---
+++ Day 2/day2.py
@@ -0,0 +1,17 @@
+from os.path import dirname, join
+
+def main():
+    current_dir = dirname(__file__)
+    file_path = join(current_dir, "input.txt")
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    print(sum({
+        "A X": 4, "A Y": 8, "A Z": 3, "B X": 1, "B Y": 5, "B Z": 9, "C X": 7, "C Y": 2, "C Z": 6
+    } [line.strip()] for line in lines))
+    print(sum({
+        "A X": 3, "A Y": 4, "A Z": 8, "B X": 1, "B Y": 5, "B Z": 9, "C X": 2, "C Y": 6, "C Z": 7
+    } [line.strip()] for line in lines))
+
+if __name__ == '__main__':
+    main()
